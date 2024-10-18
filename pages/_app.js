318e4f5970a2d76a1ec0b4bc70c1069e4b067452
Zander Picon <zander@zanderp25.com>
2024-10-18 16:21:55 -0400

    disabled app because of security reasons

--- pages/_app.js
+++ pages/_app.js
@@ -4,4 +4,8 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+function blocked({ Component, pageProps }) {
+  return <p>no</p>;
+}
+
+export default blocked
